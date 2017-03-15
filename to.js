b81bf9699a5c8be9c6f605e25ff6b9c05f57e866
livecc <cc_@live.com>
2017-03-15 17:41:43 +0800

    1

---
+++ to.js
@@ -0,0 +1,9 @@
+toQuery = (obj) => {
+    var str = "";
+    for (var i in obj) {
+        if (obj[i] || obj[i] == 0) {
+            str += (i + "=" + obj[i] + "&")
+        }
+    }
+    return str.substr(0, str.length - 1)
+}
