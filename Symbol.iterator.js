d74e8ce77bca77564e83a21f16779dfae83ca432
panghongye <cc_@live.com>
2017-07-12 18:05:12 +0800

    0

---
+++ Symbol.iterator.js
@@ -0,0 +1,28 @@
+class RangeIterator {
+  constructor(start, stop) {
+    this.value = start;
+    this.stop = stop;
+  }
+
+  [Symbol.iterator]() { return this; }
+
+  next() {
+    var value = this.value;
+    if (value < this.stop) {
+      this.value++;
+      return {done: false, value: value};
+    }
+    return {done: true, value: undefined};
+  }
+}
+
+// function range(start, stop) {
+//   return new RangeIterator(start, stop);
+// }
+
+// for (var value of range(0, 3)) {
+//   console.log(value); // 0, 1, 2
+// }
+
+
+console.log(new RangeIterator(0,3))
