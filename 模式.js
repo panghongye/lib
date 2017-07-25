143f83fdbd5108c46a6df7841345fef1ca595c0e
panghongye <cc_@live.com>
2017-07-25 14:39:25 +0800

    模式

---
+++ 模式.js
@@ -0,0 +1,43 @@
+class StateTracker {
+  constructor() {
+    this.observers = [];
+    this.internalState = 10;
+  }
+
+  // 改变内部状态，触发状态的观察者列表
+  change(val) {
+    this.internalState = val;
+    this.observers.forEach(observer => observer(val));
+  }
+
+  // 注册观察者
+  registerObserver(ObserverFn) {
+    this.observers.push(ObserverFn)
+  }
+}
+
+
+class PubSubHandler {
+  constructor() {
+    this.eventPool = {};
+  }
+
+  //移除
+  off(topicName) {
+    delete this.eventPool[topicName]
+  }
+
+  //发布
+  trigger(topicName, ...args) {
+    this.eventPool[topicName] && this.eventPool[topicName].forEach(callback => callback(...args));
+  }
+
+  //订阅
+  on(topicName, callback) {
+    let topic = this.eventPool[topicName];
+    if (!topic) {
+      this.eventPool[topicName] = []
+    }
+    this.eventPool[topicName].push(callback)
+  }
+}
