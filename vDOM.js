9aedb3a126e1d5371816c3a6ac352063b8c13f8e
panghongye <cc_@live.com>
2017-07-25 17:24:09 +0800

    vdom

---
+++ vDOM.js
@@ -0,0 +1,49 @@
+class Element {
+  constructor(tagName, props, children) {
+    this.tagName = tagName
+    this.props = props
+    this.children = children
+  }
+
+  render() {
+    var el = document.createElement(this.tagName) // 根据tagName构建
+    var props = this.props
+
+    for (var propName in props) { // 设置节点的DOM属性
+      var propValue = props[propName]
+      el.setAttribute(propName, propValue)
+    }
+
+    var children = this.children || []
+
+    children.forEach(function (child) {
+      var childEl = (child instanceof Element)
+        ? child.render() // 如果子节点也是虚拟DOM，递归构建DOM节点
+        : document.createTextNode(child) // 如果字符串，只构建文本节点
+      el.appendChild(childEl)
+    })
+
+    return el
+  }
+
+
+}
+
+// module.exports = function (tagName, props, children) {
+//   return new Element(tagName, props, children)
+// }
+
+el = (tagName, props, children) => new Element(tagName, props, children);
+
+// test
+
+ul = el('ul', { id: 'list' }, [
+  el('li', { class: 'item' }, ['Item 1']),
+  el('li', { class: 'item' }, ['Item 2']),
+  el('li', { class: 'item' }, ['Item 3'])
+])
+
+console.log(ul)
+
+ulRoot = ul.render()
+document.body.appendChild(ulRoot)
